abe84070944d52204cac6fc9b915ac4d87cc9e9e
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-11 00:03:26 -0700

    Time: 11 ms (83.01%), Space: 20.6 MB (76.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,23 +2,23 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # ["eat","tea","tan","ate","nat","bat"] -> [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-        # method: sort strings, return ana
-        original_strs = strs.copy()
-        mp = defaultdict(list) # map of sorted str and list of indexes in original array
-        for i,s in enumerate(strs):
-            sorted_s = sorted(s) # ['a', 'e', 't']
-            sorted_s = "".join(sorted_s) 
-            print(sorted_s)
-            mp[sorted_s].append(i)
-        # print(mp)
-        output = []
-        for indices_list in mp.values():
-            curr_list = []
-            for i in indices_list:
-                curr_list.append(original_strs[i])
-            output.append(curr_list)
-        return output
+        # method: dict
+        words = defaultdict(list)
+        for word in strs:
+            words["".join(sorted(list(word)))].append(word) 
+        return list(words.values())
 
-        # print(original_strs)
-        # print(strs)
-        # for i,s in enumerate(strs)
+
+
+        # ### Amazon 
+        # freqs = [Counter(el) for el in strs]
+        # groups = []
+        # existing_counters = []
+        # res = defaultdict(list)
+        
+        # for str in strs:
+        #     A = [0] * 26
+        #     for c in str:
+        #        A[ord(c) - ord('a')] += 1
+        #     res[tuple(A)].append(str)
+        # return list(res.values())
