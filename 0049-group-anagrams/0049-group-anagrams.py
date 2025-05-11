e372966ae863a2390d0412ef12289f76c14f50a5
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-11 00:03:45 -0700

    Time: 40 ms (7.69%), Space: 29 MB (5.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,22 +3,22 @@ class Solution:
         # ["eat","tea","tan","ate","nat","bat"] -> [["bat"],["nat","tan"],["ate","eat","tea"]]
 
         # method: dict
-        words = defaultdict(list)
-        for word in strs:
-            words["".join(sorted(list(word)))].append(word) 
-        return list(words.values())
+        # words = defaultdict(list)
+        # for word in strs:
+        #     words["".join(sorted(list(word)))].append(word) 
+        # return list(words.values())
 
 
 
         # ### Amazon 
-        # freqs = [Counter(el) for el in strs]
-        # groups = []
-        # existing_counters = []
-        # res = defaultdict(list)
+        freqs = [Counter(el) for el in strs]
+        groups = []
+        existing_counters = []
+        res = defaultdict(list)
         
-        # for str in strs:
-        #     A = [0] * 26
-        #     for c in str:
-        #        A[ord(c) - ord('a')] += 1
-        #     res[tuple(A)].append(str)
-        # return list(res.values())
+        for str in strs:
+            A = [0] * 26
+            for c in str:
+               A[ord(c) - ord('a')] += 1
+            res[tuple(A)].append(str)
+        return list(res.values())
