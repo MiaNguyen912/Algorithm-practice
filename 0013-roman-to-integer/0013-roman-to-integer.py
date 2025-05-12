547729442ceadf385c5c373357a3c5808a2f783d
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-12 02:44:27 -0700

    Time: 0 ms (100%), Space: 17.7 MB (64.68%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -10,17 +10,26 @@ class Solution:
         # if char is 'L' or 'C' and prev_char is 'X' -> add -20
         # if char is 'D' or 'M' and prev_char is 'C' -> add -200
         # => O(n) time, O(1) space
+        # hashmap = {'I':1, 'V':5, 'X':10, 'L':50,'C':100, 'D':500, 'M':1000}
+        # sum = hashmap[s[0]]
+        # for i,c in enumerate(s[1:], start=1):
+        #     sum += hashmap[c]
+        #     if c in "VX" and s[i-1] == "I":
+        #         sum -= 2
+        #     elif c in "LC" and s[i-1] == "X":
+        #         sum -= 20
+        #     elif c in "DM" and s[i-1] == "C":
+        #         sum -= 200
+        #     print(sum, hashmap[c])
+        # return sum
 
-        hashmap = {'I':1, 'V':5, 'X':10, 'L':50,'C':100, 'D':500, 'M':1000}
-        sum = hashmap[s[0]]
-        for i,c in enumerate(s[1:], start=1):
-            sum += hashmap[c]
-            if c in "VX" and s[i-1] == "I":
-                sum -= 2
-            elif c in "LC" and s[i-1] == "X":
-                sum -= 20
-            elif c in "DM" and s[i-1] == "C":
-                sum -= 200
-            print(sum, hashmap[c])
-        return sum
-
+        # M2: apply general rule of roman number
+        symbol = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000, '&' : 0}
+        s += '&'
+        result = 0
+        for i in range(len(s)-1):
+            if symbol[s[i]] >= symbol[s[i+1]] :
+                result += symbol[s[i]]
+            else:
+                result -= symbol[s[i]]
+        return result
