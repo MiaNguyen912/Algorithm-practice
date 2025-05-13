5cc3838b7e4092fada70129de8812a79362d0e63
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-13 00:51:49 -0700

    Time: 91 ms (25.25%), Space: 22.6 MB (58.18%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -3,6 +3,7 @@ class Solution:
         # keep a max heap of distance of points to 0
         # for each point: if heap still has space -> add point, 
         #                   else compare the distance of it to 0 and the max item in heap
+        # => O(n) time, O(k) space
         max_h = []
         for p in points:
             distance = math.sqrt(p[0]**2 + p[1]**2)
@@ -13,4 +14,7 @@ class Solution:
                 if distance < max_distance_in_h:
                     heappop(max_h)
                     heappush(max_h, [-distance, p[0], p[1]])
-        return [[x,y] for d,x,y in max_h]
+        return [[x,y] for [d,x,y] in max_h]
+
+        # M2: using heap and lambda
+        return heapq.nsmallest(k, points, key=lambda p: p[0] ** 2 + p[1] ** 2)
