e4003dcd89b1d816b8643c48cac8e6d07e82d1f3
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-15 23:08:54 -0700

    Time: 0 ms (100%), Space: 17.6 MB (89.8%) - LeetHub

---
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,50 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        # M1: bruteforce (O(n.m) time, O(1) space)
+        # count = 0 
+        # while students:
+        #     if students[0] == sandwiches[0]:
+        #         students.pop(0)
+        #         sandwiches.pop(0)
+        #         count = 0
+        #     else:
+        #         last = students.pop(0)
+        #         students.append(last)
+        #         count += 1
+        #     if count == len(students):
+        #         break
+        # return len(students)
+
+
+        # M2:  O(n) time, O(1) space
+        # count the number of students want round and square sandwich respectively
+        # for each sandwich in the sandwiches stack: (the current sandwich in each iteration is the one on top)
+        #   if there's still student who want that sandwich, decrement the count of the associated student counter
+        #   else: no one want the current sandwich -> return the number of student left
+        countStudents = [0,0] # countStudents[0] is count of round student, countStudents[1] is count of square student
+        for s in students:
+            countStudents[s] += 1
+        for sandwich in sandwiches:
+            if countStudents[sandwich] != 0:
+                countStudents[sandwich] -= 1
+            else: # no one else wants that sandwich type
+                break
+        return countStudents[0] + countStudents[1] 
+
+
+
+
+
+        # M3: 
+        # counts = [0, 0]
+        # for student in students:
+        #     counts[student] += 1
+        # remaining = len(sandwiches)
+        # for sandwich in sandwiches:
+        #     if counts[sandwich] == 0:
+        #         break
+        #     if remaining == 0:
+        #         break
+        #     counts[sandwich] -= 1
+        #     remaining -= 1
+        # return remaining
