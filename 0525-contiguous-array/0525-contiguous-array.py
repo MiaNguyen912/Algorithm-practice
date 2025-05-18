cd379c480561c901dcc5ed0720ac644310a045a6
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-18 16:52:53 -0700

    Time: 165 ms (5.16%), Space: 23.4 MB (49.38%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        #
+        mp = defaultdict(lambda: len(nums))
+        a=b=0
+        pre = 0
+        for i in range(len(nums)):
+            pre += nums[i]
+            diff = 2*pre - (i+1)
+            if diff == 0:
+                a,b = 0,i
+            elif diff in mp and (i - (mp[diff] + 1)) > b-a: # find max
+                a = mp[diff] + 1
+                b = i
+            mp[diff] = min(mp[diff], i)
+            # print(nums[a:b+1]) # slicing is [a,b) so need to +1 to b
+        return b-a+1 if a!=b else 0
