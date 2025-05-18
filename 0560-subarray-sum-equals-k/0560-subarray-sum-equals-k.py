42f2bd7ccfa65ef46b804c974329eec4367d8b06
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-17 18:26:20 -0700

    Time: 54 ms (5.2%), Space: 22.4 MB (5.72%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # prefix sum
+        # generate a prefix sum array of nums with pre[i] = sum of items in nums upto index i
+        # for each pre[i], we'd want to find how many indices x s.t pre[x] = pre[i] - k and x <= i
+        # => we'll use a map to record number of occurence of items in pre upto the current i
+        count = 0
+        pre = [0] * len(nums)
+        for i in range(len(nums)):
+            if i == 0:
+                pre[i] = nums[i]
+            else:
+                pre[i] = pre[i-1] + nums[i]
+        mp = defaultdict(int)
+        mp[0] = 1
+        for i in range(len(nums)):
+            remainder = pre[i] - k
+            count += mp[remainder]
+            mp[pre[i]] += 1
+        return count
+
