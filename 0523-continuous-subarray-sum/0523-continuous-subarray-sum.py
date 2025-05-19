f098226357f0776105701a53be46eb5d5e9155bc
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-18 19:07:26 -0700

    Time: 119 ms (6.02%), Space: 38 MB (71.75%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -3,18 +3,27 @@ class Solution:
         # good subarr:length >= 2, sum of the elements of the subarray is a multiple of k.
         # 0 is always a multiple of k
         # nums[i] >= 0
-        pre = [nums[0]]*len(nums)
-        mp = defaultdict(int)
-        # mp[0] = 1
-        # if len(nums) < 2:
-        #     return False
-        
-        for i in range(1,len(nums)):
-            pre[i] = (pre[i-1] + nums[i]) #can so %k here to make pre[] a list of remainder 
-        # print(pre)
+        # pre = [nums[0]]*len(nums)
+        # mp = defaultdict(int)      
+        # for i in range(1,len(nums)):
+        #     pre[i] = (pre[i-1] + nums[i]) #can so %k here to make pre[] a list of remainder 
+        # for i in range(len(nums)):
+        #     remainder = pre[i] % k
+        #     if remainder == 0 and i > 0: 
+        #         return True
+        #     elif remainder in mp and i > 0:
+        #         if i - mp[remainder] >= 2:
+        #             return True
+        #     else:
+        #         mp[remainder] = i
+        # return False
 
+        # better M:
+        pre = 0
+        mp = defaultdict(int)      
         for i in range(len(nums)):
-            remainder = pre[i] % k
+            pre += nums[i]
+            remainder = pre % k
             if remainder == 0 and i > 0: 
                 return True
             elif remainder in mp and i > 0:
@@ -22,5 +31,4 @@ class Solution:
                     return True
             else:
                 mp[remainder] = i
-            # print(mp)
         return False
