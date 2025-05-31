f76d9e7c89af9a7c7d0f2475f7f9cba801cb3434
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 03:14:27 -0700

    Time: 0 ms (100%), Space: 17.7 MB (53.3%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        # valid: min 3 chars
+        #       0-9, a-z, A-Z
+        #       at least 1 vowel, 1 consonant
+        if len(word)<3:
+            return False
+        vowel = ['a', 'e', 'i', 'o', 'u']
+        has_vowel = has_consonant = False
+        for c in word:
+            if not c.isalnum():
+                return False
+            if c.lower() in vowel:
+                has_vowel = True
+            else:
+                if c.isalnum():
+                    if c.isalpha():
+                        has_consonant = True
+        return has_vowel and has_consonant
+        
