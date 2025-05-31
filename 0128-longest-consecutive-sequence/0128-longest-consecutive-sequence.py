698b738f3cc8efce4fde2ba01dba8cf12582c8ea
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 16:48:31 -0700

    Time: 63 ms (30.79%), Space: 34.2 MB (68.1%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,22 +3,38 @@ class Solution:
         # convert nums to set to remove duplicate
         # for each number, while num+1 is in set, increase num and check if num+1 in in set, also remove that num from set to avoid looping through it again
 
+        # O(n), since we only pass each number once
         nums_set = set(nums)
         max_len = 0
-        curr_len = 0
         for num in nums:
-            if num in nums_set:
-                curr_len = 0
-                d = num
-                while num in nums_set:
+            if num in nums_set: # check if num hasn't been removed from set
+                curr_len = 0 # reset
+                temp = num 
+                while num in nums_set: # check all numbers consecutive to num (on the right side, including num)
                     curr_len += 1
-                    nums_set.remove(num)
-                    num += 1
-                num = d-1
-                while num in nums_set:
+                    nums_set.remove(num) # remove from set so we wont need to consider this number again later
+                    num += 1 # increment num
+                num = temp-1 # reset 
+                while num in nums_set: # check all numbers consecutive to num (on the left side)
                     curr_len += 1
                     nums_set.remove(num)
                     num -= 1
                 max_len = max(max_len, curr_len)
         return max_len
                 
+
+
+        #-----------------------
+        # key: a number from the input array, 
+        # value: length of a consecutive sequence with that number as either the upper or lower bound of the sequence.
+        nums = set(nums)
+        table = {}
+        maxval = 0
+        for num in nums:
+            x = table.get(num - 1, 0) # get len of consecutive sequence that end at num-1
+            y = table.get(num + 1, 0) # get len of consecutive sequence that begins at num+1
+            val = x + y + 1 # combine the 2 sequences above and include num
+            table[num - x] = val # store the len of sequence at the beginning of sequence
+            table[num + y] = val # store the len of sequence at the end of sequence
+            maxval = max(maxval, val) 
+        return maxval
