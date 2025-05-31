8118d3145b39ec60670214cfeb21ad5949cbd5b3
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 01:32:15 -0700

    Time: 64 ms (61.84%), Space: 18.3 MB (81.53%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -4,15 +4,14 @@ class Solution:
         # flip: changing 0 to 1
         # -> find longest subarr with number of 0 <= k
 
-        num_0 = 0
         l = 0
         max_len = 0
         for r in range(len(nums)):
             if nums[r] == 0:
-                num_0 += 1
-            while num_0 > k: # invalid window -> cut left
+                k -= 1
+            while k < 0: # invalid window -> cut left
                 if nums[l] == 0:
-                    num_0 -= 1
+                    k += 1
                 l += 1
             # at this point, we have the longest valid window that end at 0
             max_len = max(max_len, r-l+1)
