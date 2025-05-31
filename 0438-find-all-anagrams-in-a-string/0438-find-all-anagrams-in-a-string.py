227f8d538c3de40a4de0dbe3e8d9931d9759da0d
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 01:47:13 -0700

    Time: 6822 ms (7.8%), Space: 18.2 MB (79.92%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,13 +2,12 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         # return array of all start indices of p's anagrams in s (in any order)
         # -> use a sliding window len = len(p), check if the substr in window is an anagram of p
-        l,r = 0,len(p)
+        r = len(p)
         p = sorted(p)
         output = []
         while r < len(s)+1:
-            if sorted(s[l:r]) == p:
-                output.append(l)
-            l += 1
+            if sorted(s[r-len(p):r]) == p:
+                output.append(r-len(p))
             r += 1
         return output
 
