8d0c4d81d3b63335760540d369c0bd63abdddce1
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 00:05:58 -0700

    Time: 63 ms (93.94%), Space: 29.3 MB (23.3%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
@@ -9,39 +9,39 @@ class Solution:
         # -> sum(nums[l->r]) = sum(nums) - x
         # => find longest subarr l->r such that sum(nums[l->r]) = sum(nums) - x = target
         # => use prefix sum (pre[r] - pre[l-1] = target)
-        pre_r = 0
-        target = sum(nums) - x
-        if target == 0: # if x is sum of all items, we need to remove len(nums) items
-            return len(nums)
-        mp = defaultdict(lambda: math.inf)  # map stores pairs <sum[subarr[0->i]] : i> (i is the leftmost possible index)
-        mp[0] = -1 # store pair <sum([]) : -1>
-        max_len = 0 
 
-        for r in range(len(nums)):
-            pre_r += nums[r]
-            pre_l = pre_r - target
-            if pre_l in mp:
-                max_len = max(max_len, r - mp[pre_l])
-            mp[pre_r] = min(mp[pre_r], r)
-        if max_len == 0:
-            return -1
-        return len(nums) - max_len
-
-
-
+        # pre_r = 0
         # target = sum(nums) - x
-        # n = len(nums)
-        # if target < 0:
+        # if target == 0: # if x is sum of all items, we need to remove len(nums) items
+        #     return len(nums)
+        # mp = defaultdict(lambda: math.inf)  # map stores pairs <sum[subarr[0->i]] : i> (i is the leftmost possible index)
+        # mp[0] = -1 # store pair <sum([]) : -1>
+        # max_len = 0 
+        # for r in range(len(nums)):
+        #     pre_r += nums[r]
+        #     pre_l = pre_r - target
+        #     if pre_l in mp:
+        #         max_len = max(max_len, r - mp[pre_l])
+        #     mp[pre_r] = min(mp[pre_r], r)
+        # if max_len == 0:
         #     return -1
-        # if target == 0:
-        #     return n
-        # left = ans = 0
-        # sum_ = 0
-        # for right, x in enumerate(nums):
-        #     sum_ += x
-        #     while sum_ > target:
-        #         sum_ -= nums[left]
-        #         left += 1
-        #     if sum_ == target:
-        #         ans = max(right - left + 1, ans)
-        # return n - ans if ans != 0 else -1
+        # return len(nums) - max_len
+
+
+        #--------------------------
+        # M2: sliding window (faster)
+        target = sum(nums) - x
+        if target < 0:
+            return -1
+        if target == 0:
+            return len(nums)
+        l = max_len = 0
+        window_sum = 0
+        for r in range(len(nums)):
+            window_sum += nums[r]
+            while window_sum > target: # cut left
+                window_sum -= nums[l]
+                l += 1
+            if window_sum == target:
+                max_len = max(r - l + 1, max_len)
+        return len(nums) - max_len if max_len != 0 else -1
