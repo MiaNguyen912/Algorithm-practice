72af4edb913da60b3e1bff2772c0794d0f2d4610
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 02:09:28 -0700

    Time: 291 ms (10.24%), Space: 18.2 MB (23.39%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # s: all uppercase letters
+        # can flip at most k chars to other char (uppercase)
+        # find longest substr containing same letter after at most k flips
+        # => find longest substr with num of other chars besides the majority char <= k
+        # -> sliding window, use a dict to store <char:count> 
+
+        mp = defaultdict(int)
+        max_len = 0
+        l = 0
+        for r,c in enumerate(s):
+            mp[c] += 1
+            all_counts = mp.values()
+            majority_count = max(all_counts)
+            while sum(mp.values()) - max(mp.values()) > k and l<=r: # invalid window, cut left
+                mp[s[l]] -= 1
+                l += 1
+            max_len = max(max_len, r-l+1)
+        return max_len
+        
