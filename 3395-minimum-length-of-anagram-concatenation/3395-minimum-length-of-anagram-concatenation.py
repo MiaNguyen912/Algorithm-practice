fb5b53387123641d738a6b033ca2bb3ab9a7167d
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 13:54:57 -0700

    Time: 1225 ms (31.85%), Space: 19.1 MB (32.48%) - LeetHub

--- 3395-minimum-length-of-anagram-concatenation/3395-minimum-length-of-anagram-concatenation.py
+++ 3395-minimum-length-of-anagram-concatenation/3395-minimum-length-of-anagram-concatenation.py
@@ -4,34 +4,34 @@ class Solution:
         # find min possible length of t
         
         # M1: brute force, increase the anagram size from 1->n to check (O(n^2 logn))
-        s = list(s)
-        n = len(s)
-        for size in range(1, n):
-            if n % size == 0: # only care if n % size_of_anagram == 0
-                anagram = sorted(s[:size])
-                for j in range(size, n, size): # step of size
-                    temp = sorted(s[j:j+size])
-                    if anagram != temp: 
-                        break
-                else:
-                    return size         
-        return n
+        # s = list(s)
+        # n = len(s)
+        # for size in range(1, n):
+        #     if n % size == 0: # only care if n % size_of_anagram == 0
+        #         anagram = sorted(s[:size])
+        #         for j in range(size, n, size): # step of size
+        #             temp = sorted(s[j:j+size])
+        #             if anagram != temp: 
+        #                 break
+        #         else:
+        #             return size         
+        # return n
 
 
         # M2: same as M1, but using Counter() to get the hashable value of string instead of sort() it (O(n^2))
-        # s = list(s)
-        # n = len(s)
-        # ans = n
-        # for size in range(1, n):
-        #     if n % size == 0: 
-        #         count = Counter(s[:size]) # O(n)
-        #         for j in range(size, n, size):
-        #             temp = Counter(s[j:j+size])
-        #             if count != temp:
-        #                 break
-        #         else:
-        #             return size
-        # return ans
+        s = list(s)
+        n = len(s)
+        ans = n
+        for size in range(1, n):
+            if n % size == 0: 
+                count = Counter(s[:size]) # O(n)
+                for j in range(size, n, size):
+                    temp = Counter(s[j:j+size])
+                    if count != temp:
+                        break
+                else:
+                    return size
+        return ans
 
 
 
