8d43f781565f39ad607909cedc392e4337959573
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 03:15:53 -0700

    Time: 1 ms (28.25%), Space: 17.9 MB (10.93%) - LeetHub

--- 3396-valid-word/3396-valid-word.py
+++ 3396-valid-word/3396-valid-word.py
@@ -5,12 +5,13 @@ class Solution:
         #       at least 1 vowel, 1 consonant
         if len(word)<3:
             return False
+        word = word.lower()
         vowel = ['a', 'e', 'i', 'o', 'u']
         has_vowel = has_consonant = False
         for c in word:
             if not c.isalnum():
                 return False
-            if c.lower() in vowel:
+            if c in vowel:
                 has_vowel = True
             else:
                 if c.isalpha():
