a204f1e05e8ed3c06794bb505ded60c30a7f8036
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 03:53:02 -0700

    Time: 79 ms (65.49%), Space: 26.9 MB (33.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,12 +3,14 @@ class Solution:
         # prices[i] is the price on the ith day.
         # want to maximize profit by choosing a single day to buy 1 stock and a different day in the future to sell it
         # return max profit or 0
-        # => for each r, find a min l, then compare this with max_profit
+        # => sliding window: for each r, find a min l, then compare this with max_profit
 
         max_profit = 0
         min_l = prices[0]
         for curr_price in prices:
             if curr_price > min_l:
                 max_profit = max(max_profit, curr_price - min_l)
-            min_l = min(min_l, curr_price)
+            else:
+                min_l = min(min_l, curr_price)
         return max_profit 
+
