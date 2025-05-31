e42ee1e60959450514f582032bcdd2f9c9734add
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-30 23:59:28 -0700

    Time: 259 ms (7.02%), Space: 40.7 MB (8.08%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
@@ -2,23 +2,20 @@ class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         # In one operation, either remove the leftmost or rightmost element from nums and subtract its value from x. 
         # Return the min number of operations to reduce x to 0 if it is possible, otherwise, return -1.
-        # all 
 
+
+        # M1: prefix sum
         # let l, r be the range of subarr after removed element to reduce x to 0
         # -> sum(nums[l->r]) = sum(nums) - x
         # => find longest subarr l->r such that sum(nums[l->r]) = sum(nums) - x = target
-        # => prefix sum (pre[r] - pre[l-1] = target)
-
-        # pre = [nums[0]] * len(nums)
-        # for i in range(1,len(nums)):
-        #     pre[i] = pre[i-1] + nums[i]
+        # => use prefix sum (pre[r] - pre[l-1] = target)
         pre_r = 0
         target = sum(nums) - x
         if target == 0: # if x is sum of all items, we need to remove len(nums) items
             return len(nums)
         mp = defaultdict(lambda: math.inf)  # map stores pairs <sum[subarr[0->i]] : i> (i is the leftmost possible index)
         mp[0] = -1 # store pair <sum([]) : -1>
-        max_len = -1 
+        max_len = 0 
 
         for r in range(len(nums)):
             pre_r += nums[r]
@@ -26,7 +23,7 @@ class Solution:
             if pre_l in mp:
                 max_len = max(max_len, r - mp[pre_l])
             mp[pre_r] = min(mp[pre_r], r)
-        if max_len == -1:
+        if max_len == 0:
             return -1
         return len(nums) - max_len
 
