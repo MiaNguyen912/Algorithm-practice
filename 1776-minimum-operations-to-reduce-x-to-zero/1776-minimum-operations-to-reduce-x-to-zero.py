5cb8dd6e3f3b8c50bf865e6a46404b6198f91953
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-30 23:57:24 -0700

    Time: 272 ms (6.6%), Space: 40.7 MB (10.32%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
@@ -9,23 +9,42 @@ class Solution:
         # => find longest subarr l->r such that sum(nums[l->r]) = sum(nums) - x = target
         # => prefix sum (pre[r] - pre[l-1] = target)
 
-        pre = [nums[0]] * len(nums)
-        for i in range(1,len(nums)):
-            pre[i] = pre[i-1] + nums[i]
-        target = pre[-1] - x
+        # pre = [nums[0]] * len(nums)
+        # for i in range(1,len(nums)):
+        #     pre[i] = pre[i-1] + nums[i]
+        pre_r = 0
+        target = sum(nums) - x
         if target == 0: # if x is sum of all items, we need to remove len(nums) items
             return len(nums)
         mp = defaultdict(lambda: math.inf)  # map stores pairs <sum[subarr[0->i]] : i> (i is the leftmost possible index)
         mp[0] = -1 # store pair <sum([]) : -1>
         max_len = -1 
 
-        # print("target: ", target)
         for r in range(len(nums)):
-            pre_l = pre[r] - target
+            pre_r += nums[r]
+            pre_l = pre_r - target
             if pre_l in mp:
                 max_len = max(max_len, r - mp[pre_l])
-                # print("max_len: ", max_len)
-            mp[pre[r]] = min(mp[pre[r]], r)
+            mp[pre_r] = min(mp[pre_r], r)
         if max_len == -1:
             return -1
         return len(nums) - max_len
+
+
+
+        # target = sum(nums) - x
+        # n = len(nums)
+        # if target < 0:
+        #     return -1
+        # if target == 0:
+        #     return n
+        # left = ans = 0
+        # sum_ = 0
+        # for right, x in enumerate(nums):
+        #     sum_ += x
+        #     while sum_ > target:
+        #         sum_ -= nums[left]
+        #         left += 1
+        #     if sum_ == target:
+        #         ans = max(right - left + 1, ans)
+        # return n - ans if ans != 0 else -1
