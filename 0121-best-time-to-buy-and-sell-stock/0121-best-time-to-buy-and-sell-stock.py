52d6692272bc6e9d189fb73395707a4f9a95c71d
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 03:44:19 -0700

    Time: 127 ms (29.73%), Space: 27 MB (8.91%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # prices[i] is the price on the ith day.
+        # want to maximize profit by choosing a single day to buy 1 stock and a different day in the future to sell it
+        # return max profit or 0
+        # => for each r, find a min l, then compare this with max_profit
+
+        max_profit = 0
+        min_l = prices[0]
+        for r,curr_price in enumerate(prices):
+            if curr_price > min_l:
+                max_profit = max(max_profit, curr_price - min_l)
+            min_l = min(min_l, curr_price)
+        return max_profit 
