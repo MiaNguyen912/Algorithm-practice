78f3a636954a1405301589d9b2795b2874abd674
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-05-31 21:24:22 -0700

    Time: 11 ms (87.07%), Space: 18.1 MB (7.28%) - LeetHub

--- 3681-maximum-area-rectangle-with-point-constraints-i/3681-maximum-area-rectangle-with-point-constraints-i.py
+++ 3681-maximum-area-rectangle-with-point-constraints-i/3681-maximum-area-rectangle-with-point-constraints-i.py
@@ -6,22 +6,16 @@ class Solution:
         #           parallel to axes
         # Return maximum area or -1
 
+        # M1: bruteforce (O(n^3))
         # sort points by x
-        # for points of same x, check if there's any point (called a) with a's y in the middle of their min and max y
-        #   if a has same x as those point
-
-
-
-        # bruteforce: for each point, for another point, check if these 2 points can form a rectangle, if yes, check if another points in inside this square
-
-
+        # for each 2 points, check if these 2 points can form a rectangle (are on a diagonal)
+        # if yes, check if there's any other points in inside this rect
         if len(points) < 4:
             return -1
-        points = sorted(points) 
-
-        print(points)
+        points = sorted(points) # [[1, 1], [1, 3], [3, 1], [3, 3]]
         points_set = set([tuple(p) for p in points]) # change to set since `if i in points_set` is O(1)
-        print(points_set)
+        print(points_set) # {(3, 1), (1, 1), (3, 3), (1, 3)}
+
         max_area = -1
         for i in range(len(points) - 3):
             for j in range(i + 3, len(points)):
@@ -32,39 +26,10 @@ class Solution:
                 area = (x2 - x1) * (y2 - y1)  
                 if area <= max_area: # if area isn't max, no need to check points
                     continue
-                corners = [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
-                if (((x1, y2) in points_set) and 
-                    ((x2, y1) in points_set) and
+                corners = [(x1, y1), (x1, y2), (x2, y1), (x2, y2)] #corners of the rect containing (x1, y1),(x2, y2) 
+                if (((x1, y2) in points_set) and ((x2, y1) in points_set) and
                     not any(x1 <= x <= x2 and y1 <= y <= y2 and (x, y) not in corners for x, y in points[i + 1: j])
                 ):
                     max_area = max(area, max_area)
         return max_area
 
-
-
-
-
-
-        # if len(points) < 4:
-        #     return -1
-        # points = sorted(points) # [(1, 1), (1, 2), (1, 3), (3, 1), (3, 2), (3, 3)]
-        # print(points)
-
-        # # points_set = set([tuple(p) for p in points])
-        # max_area = -1
-        # for i in range(len(points) - 3):
-        #     for j in range(i + 3, len(points)):
-        #         x1, y1 = points[i]
-        #         x2, y2 = points[j]
-        #         if x1 == x2 or y1 == y2:  # if not diagonal
-        #             continue
-        #         area = (x2 - x1) * (y2 - y1)  
-        #         if area <= max_area: # if area isn't max, no need to check points
-        #             continue
-        #         corners = [[x1, y1], [x1, y2], [x2, y1], [x2, y2]]
-        #         if (([x1, y2] in points) and 
-        #             ([x2, y1] in points) and
-        #             not any(x1 <= x <= x2 and y1 <= y <= y2 and [x, y] not in corners for x, y in points[i + 1: j])
-        #         ):
-        #             max_area = max(area, max_area)
-        # return max_area
