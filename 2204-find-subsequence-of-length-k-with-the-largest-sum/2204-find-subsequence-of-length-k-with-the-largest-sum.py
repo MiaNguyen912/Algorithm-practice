3f607a0ce73c92b1589a5559a78f5524ea1dadbf
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-02 03:18:18 -0700

    Time: 7 ms (30.28%), Space: 18.1 MB (49.8%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -4,11 +4,20 @@ class Solution:
         # => find k largest items while maintaining their relative order
         # M1: store all (value, index) in max_heap -> pop k times to get k largest items
         # -> sort them by index => O(nlogn)
+        max_h = []
+        for i,num in enumerate(nums):
+            heappush(max_h, (-num,i)) # sorted by desc num, asc i
+        top_k = []
+        for i in range(k):
+            top_k.append(heappop(max_h))
+        top_k.sort(key=lambda pair:pair[1])
+        return [-val for val,i in top_k]
+        
 
         # M2: build a list of (val,index), sorted by val, then get the top k items 
         # -> sort again by index => O(nlogn)
-        pairs = [(value, idx) for idx, value in enumerate(nums)]
-        pairs.sort(key=lambda pair: (pair[0], pair[1])) # [(1, 1), (2, 0), (3, 2), (3, 3)]
-        top_k = pairs[-k:]
-        top_k.sort(key=lambda pair : pair[1])
-        return [val for val,index in top_k]
+        # pairs = [(value, idx) for idx, value in enumerate(nums)]
+        # pairs.sort(key=lambda pair: (pair[0], pair[1])) # [(1, 1), (2, 0), (3, 2), (3, 3)]
+        # top_k = pairs[-k:]
+        # top_k.sort(key=lambda pair : pair[1])
+        # return [val for val,index in top_k]
