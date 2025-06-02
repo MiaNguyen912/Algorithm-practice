c127e7386761c798d89991abb2e8f687dbab1a92
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-02 03:18:37 -0700

    Time: 13 ms (14.54%), Space: 18 MB (76.18%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -4,15 +4,25 @@ class Solution:
         # => find k largest items while maintaining their relative order
         # M1: store all (value, index) in max_heap -> pop k times to get k largest items
         # -> sort them by index => O(nlogn)
-        max_h = []
-        for i,num in enumerate(nums):
-            heappush(max_h, (-num,i)) # sorted by desc num, asc i
-        top_k = []
+        # max_h = []
+        # for i,num in enumerate(nums):
+        #     heappush(max_h, (-num,i)) # sorted by desc num, asc i
+        # top_k = []
+        # for i in range(k):
+        #     top_k.append(heappop(max_h))
+        # top_k.sort(key=lambda pair:pair[1])
+        # return [-val for val,i in top_k]
+        num=[-i for i in nums]
+        l=[]
+        a=[]
+        heapq.heapify(num)
         for i in range(k):
-            top_k.append(heappop(max_h))
-        top_k.sort(key=lambda pair:pair[1])
-        return [-val for val,i in top_k]
-        
+            l.insert(0,-heapq.heappop(num))
+        for i in nums:
+            if i in l:
+                l.remove(i)
+                a.append(i)
+        return a
 
         # M2: build a list of (val,index), sorted by val, then get the top k items 
         # -> sort again by index => O(nlogn)
