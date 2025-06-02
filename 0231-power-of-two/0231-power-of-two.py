3f371b4be8cbd72520aa540534082fe2cdb4b267
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-02 03:30:16 -0700

    Time: 3 ms (15.86%), Space: 17.7 MB (83.9%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,12 +1,24 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         # power of 2: must be even, > 0
-        # -2^31 <= n <= 2^31 - 1  -> x can be from 0->30 -> check if n == 2^x
+
+        # M1: -2^31 <= n <= 2^31 - 1  -> x can be from 0->30 -> check if n == 2^x
+        # if n <=0:
+        #     return False
+        # product = 1/2 # init to 1/2 so that in the first loop when x=0, product = 1
+        # for x in range(31):
+        #     product *= 2
+        #     if n == product:
+        #         return True
+        # return False
+
+        # M2: keep dividing by 2 until n == 1, if can't be 1 -> return False
         if n <=0:
             return False
-        product = 1/2 # init to 1/2 so that in the first loop when x=0, product = 1
-        for x in range(31):
-            product *= 2
-            if n == product:
-                return True
-        return False
+        while n > 1:
+            n /= 2
+        # at this point, n <= 1
+        if n < 1:
+            return False
+        return True
+
