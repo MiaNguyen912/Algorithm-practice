bc61ca07d11cbed755192269326bf4669c01ce26
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-01 23:57:36 -0700

    Time: 178 ms (5.17%), Space: 23.5 MB (50.31%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -17,14 +17,41 @@ class Solution:
         # return b-a+1 if a!=b else 0
 
         # M2:
-        mp = dict() # mp[i]
-        mp[0] = -1 # for empty array [], numbers of 0 and 1 are equal
-        pre = 0
-        result = 0 # len of result array
-        for i in range(len(nums)):
-            pre += 1 if nums[i]==1 else -1 # pre is diff of 1 and 0
-            if pre in mp:
-                result = max(result, i - mp[pre]) # find the longest subarr
+        # mp = dict() # mp[i]
+        # mp[0] = -1 # for empty array [], numbers of 0 and 1 are equal
+        # pre = 0
+        # result = 0 # len of result array
+        # for i in range(len(nums)):
+        #     pre += 1 if nums[i]==1 else -1 # pre is diff of 1 and 0
+        #     if pre in mp:
+        #         result = max(result, i - mp[pre]) # find the longest subarr
+        #     else:
+        #         mp[pre] = i # pre[i] store (num of 1s - num of 0s) upto index i
+        # return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+        count = 0
+        max_len = 0
+        mp = dict() # store <(num1-num0) : leftmost index>
+        mp[0] = -1 # for empty [], (num1-num0) == 0 -> store <0:-1>
+        diff_1_0 = 0
+        for r,curr in enumerate(nums):
+            if curr == 0:
+                diff_1_0 -= 1
             else:
-                mp[pre] = i # pre[i] store (num of 1s - num of 0s) upto index i
-        return result
+                diff_1_0 += 1
+            if diff_1_0 in mp:
+                max_len = max(max_len, r - mp[diff_1_0])
+            mp[diff_1_0] = min(mp.get(diff_1_0, r), r) # store the leftmost index
+        return max_len
