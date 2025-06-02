e7aa7b938ffdb86e4b4e5ba3327df131fb0ed858
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-02 05:00:25 -0700

    Time: 16 ms (94.07%), Space: 23.1 MB (93.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,13 +6,24 @@ class Solution:
         # postfix_product[i] is product of everything after i
         # => answer[i] = prefix_product[i] * postfix_product[i]
         # ex: nums=[1,2,3,4] -> prefix_product=[1, 1, 2, 6]; postfix_product=[24, 12, 4, 1]
+        # prefix_product = [1] * len(nums)
+        # for i in range(1,len(nums)):
+        #     prefix_product[i] = prefix_product[i-1] * nums[i-1]
+        # postfix_product = [1] * len(nums)
+        # answer = prefix_product
+        # for i in range(len(nums)-2,-1,-1):
+        #     postfix_product[i] = postfix_product[i+1] * nums[i+1]
+        #     answer[i] *= postfix_product[i]
+        # return answer
 
-        prefix_product = [1] * len(nums)
-        for i in range(1,len(nums)):
-            prefix_product[i] = prefix_product[i-1] * nums[i-1]
-        postfix_product = [1] * len(nums)
-        answer = prefix_product
-        for i in range(len(nums)-2,-1,-1):
-            postfix_product[i] = postfix_product[i+1] * nums[i+1]
-            answer[i] *= postfix_product[i]
-        return answer
+        # M1 better: use preProduct, postProduct to keep track of product upto i instead of storing in array
+        output = [1]*len(nums)
+        preProduct = 1
+        for i in range(len(nums)):
+            output[i] = preProduct
+            preProduct *= nums[i]
+        postProduct = 1
+        for i in range(len(nums)-1, -1, -1):
+            output[i] *= postProduct
+            postProduct *= nums[i]
+        return output
