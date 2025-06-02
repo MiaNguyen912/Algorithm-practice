3cc43060da0b644264c1e4ab57f4754e5037f36f
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-02 01:08:21 -0700

    Time: 55 ms (39.02%), Space: 38.2 MB (35.66%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -19,17 +19,46 @@ class Solution:
         # return False
 
         # better M:
-        pre = nums[0]
-        mp = defaultdict(int)   
-        mp[pre%k] = 0   
-        for i in range(1,len(nums)):
-            pre += nums[i]
-            remainder = pre % k
-            if remainder == 0: 
-                return True
-            elif remainder in mp:
-                if i - mp[remainder] >= 2:
+        # pre = nums[0]
+        # mp = defaultdict(int)   
+        # mp[pre%k] = 0   
+        # for i in range(1,len(nums)):
+        #     pre += nums[i]
+        #     remainder = pre % k
+        #     if remainder == 0: 
+        #         return True
+        #     elif remainder in mp:
+        #         if i - mp[remainder] >= 2:
+        #             return True
+        #     else:
+        #         mp[remainder] = i
+        # return False
+
+
+
+
+
+
+
+
+
+
+
+
+        # pre[r] - pre[l] = n*k => (pre[r]%k - pre[l]%k)%k == 0
+        # => pre[l]%k == pre[r]%k, and r-l>=2
+        # ex: [5 0 0 0],k=3 -> pre=[5 5 5 5], mp={0:-1, 2:0}, valid range is arr[1:3] (=[0,0])
+
+        pre = 0
+        mp = dict() # store <(sum of items up to i)%k : i>
+        mp[0] = -1 # <sum of [] : -1>
+        for r,curr in enumerate(nums):
+            pre += curr
+            target = pre%k
+            # print(target, r,mp)
+            if target in mp:
+                if r-mp[target] >= 2: #[5 0 0],k=3->valid range:[0 0] -> r-l>=2
                     return True
             else:
-                mp[remainder] = i
+                mp[target] = r # only store the leftmost index of mp[target]
         return False
