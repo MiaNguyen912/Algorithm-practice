dc780746456143288c254a51c713f450c11d54f6
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-02 22:29:57 -0700

    Time: 47 ms (5.51%), Space: 25 MB (87.61%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.py
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.py
@@ -6,11 +6,64 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        # U:
+        # - BST, not empty; node.val are unique
+        # - low, high are valid
+        # - find sum of values in [low, high]
+
+        # M1: dfs, preorder recursive 
         if not root: #empty
             return 0
-        elif not root.left and not root.right: # leaf
-            return root.val if low<=root.val<=high else 0
-        total = self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+        total = 0
         if root.val in range(low,high+1):
             total += root.val
+        if root.left:
+            total += self.rangeSumBST(root.left, low, high)
+        if root.right:
+            total += self.rangeSumBST(root.right, low, high)
         return total
+
+        # M2:
+
+        
+
+        # M: dfs, preorder recursive 
+        # - base case: if not root: return 0
+        # - recursive case:
+        #       init sum = 0
+        #       if root.val is in [low, high]: sum += root.val
+        #       call self.rangeSumBST() on root.left, root.right
+        # - return sum
+
+        # if not root: 
+        #     return 0
+        # sum = 0
+        # def sum_helper(root):
+        #     nonlocal sum
+        #     if not root: 
+        #         return 0
+        #     if root.val in range(low, high+1):
+        #         sum += root.val
+        #     sum_helper(root.left)
+        #     sum_helper(root.right)
+        # sum_helper(root)
+        # return sum
+
+        #---------------------------
+        # Method 2: advanced preorder
+        # - if a node.val in [low, high], add it to sum and continue traversing it's left, right
+        #   elif node.val > high: only traverse it's left
+        #   elif node.val < low: only traverse it's right
+
+        def sum_helper(root):
+            if not root: 
+                return 0
+            sum = 0
+            if root.val in range(low, high+1):
+                sum += root.val + sum_helper(root.left) + sum_helper(root.right)
+            elif root.val > high:
+                sum += sum_helper(root.left)
+            else:
+                sum += sum_helper(root.right)
+            return sum
+        return sum_helper(root)
