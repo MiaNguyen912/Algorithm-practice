b65f65ef48f20bab6f42a2fce5c31fbc0643bc1e
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-27 18:02:58 -0700

    Time: 771 ms (9.97%), Space: 25.7 MB (89.92%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -5,25 +5,13 @@ class Solution:
         # sliding window
         # for each r, find the leftmost l that satisfy
 
-
-        # prefix min/max
-        # n = len(nums)
-        # premin = [0]*(n+1)
-        # for i in range(n+1):
-        #     premin[i] = min(premin[i-1], nums[i-1])
-        # premax = [0]*(n+1)
-        #     premax[i] = max(premax[i-1], nums[i-1])
-        
-        # for r in range(n+1):
-
-
-
+        # time O(nlogn), space O(n)
         size = 0
         l=0
         sorted_l = SortedList([])
         for r in range(len(nums)):
             sorted_l.add(nums[r])
-            while abs(sorted_l[0]-sorted_l[-1]) > limit and l <r: # cut left
+            while sorted_l[-1]-sorted_l[0] > limit: # cut left
                 sorted_l.discard(nums[l])
                 l += 1
             size = max(size, r-l+1)
