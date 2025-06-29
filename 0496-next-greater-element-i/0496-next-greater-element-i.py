02b714a1871b063f2de20f666ed3caa6e568e3fb
Mia Nguyen <123523165+MiaNguyen912@users.noreply.github.com>
2025-06-28 17:42:58 -0700

    Time: 0 ms (100%), Space: 18.1 MB (20.63%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -9,36 +9,21 @@ class Solution:
 
         # plan 2: monotonic stack (O(n))
         # create the decreasing monostack to record the subarr that each item in num2 is the max of
+
+        mp = {n:i for i,n in enumerate(nums1)}
+        N = len(nums2)
         ans = [0] * (len(nums1))
         nums2 = nums2 + [math.inf]
         monostack = [] # decreasing monostack
-        max_arr = [0]*(len(nums2)-1)  # store the index of subarr range for each item in nums s.t that item is the max of the subarr
+        
         for i in range(len(nums2)):
-            l,r = 0,0
             while monostack and nums2[monostack[-1]] < nums2[i]: # while the last item in monostack < curr item
                 last_index = monostack.pop()
-                l = 0
-                if monostack:
-                    l = monostack[-1] + 1
                 r = i-1
-                max_arr[last_index] = [l,r]
-            curr_item = nums2[i]
-            # print(curr_item, [l,r])
-            # if curr_item in nums1:
-            #     print(curr_item, r, nums2[r+1)
-
-            #     ans[nums1.index(curr_item)] = nums2[r+1] if r < (len(nums2)-1) else -1
+                if nums2[last_index] in mp:
+                    ans[mp[nums2[last_index]]] = nums2[r+1] if r < N-1 else -1
             monostack.append(i)
-        print(max_arr)
         # ex: nums2 = [1,3,4,2] => max_arr = [[0, 0], [0, 1], [0, 3], [3, 3]]
-
-
-        for i,num in enumerate(nums1):
-            index = nums2.index(num)
-            if max_arr[index][1]+1 < len(nums2)-1:
-                ans[i] = nums2[max_arr[index][1]+1]
-            else:
-                ans[i] = -1
 
         return ans
 
